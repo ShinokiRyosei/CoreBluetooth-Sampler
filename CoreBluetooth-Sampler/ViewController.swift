12a9e698b846ef680ab8f43a3bcaf718d9fe7dea
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-07-26 03:12:37 +0900

    [Add] CBCentral Manager

--- CoreBluetooth-Sampler/ViewController.swift
+++ CoreBluetooth-Sampler/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate {
+    
+    var centralManager: CBCentralManager!
+    var peripheral: CBPeripheral!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func selectStartScan() {
+        centralManager.scanForPeripheralsWithServices(nil, options: nil)
+    }
+    
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        print("peripherial: \(peripheral)")
+    }
+    
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        print("state: \(central.state)")
+    }
 }
 
